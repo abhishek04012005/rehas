ebf7140de2f3ebacf393b58ce01d40d8a067d1c8
abhishek04012005 <abhishekchoudhary5414@gmail.com>
2025-12-20 18:20:44 +0530

    fdad

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,7 +6,8 @@ import styles from './navbar.module.css';
 import Image from 'next/image';
 import { rehasData } from '@/data/rehasData';
 import { navbarData } from '@/data/navbar';
-import { WhatsApp, ExpandMore, Edit } from '@mui/icons-material';
+import { WhatsApp, ExpandMore } from '@mui/icons-material';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
 
 export default function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -118,7 +119,7 @@ export default function Navbar() {
                 aria-label="Submit enquiry"
                 title="Submit enquiry"
             >
-                <Edit className={styles.enquiryIcon} />
+                <ContactSupportIcon className={styles.enquiryIcon} />
             </Link>
         </>
     );
